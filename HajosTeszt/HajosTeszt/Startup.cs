0ee5c9b2275c200a71ca7a1b4ba9c5ca7cb45ade
Anasztázia Tamás <anasztazia.tamas@stud.uni-corvinus.hu>
2021-04-18 21:07:09 +0200

    8het

--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -16,6 +16,7 @@ namespace HajosTeszt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,10 @@ namespace HajosTeszt
                 });
             });
             */
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
