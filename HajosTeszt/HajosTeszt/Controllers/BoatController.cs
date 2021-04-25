b9956a7dca68bb9023e32e2f60d5e9048a62d886
Anasztázia Tamás <anasztazia.tamas@stud.uni-corvinus.hu>
2021-04-25 23:53:10 +0200

    9változtatás

---
+++ HajosTeszt/HajosTeszt/Controllers/BoatController.cs
@@ -0,0 +1,38 @@
+﻿using HajosTeszt.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.Controllers
+{
+    public class BoatController : Controller
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public ActionResult M1()
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdések = from x in context.Questions select x.QuestionText;
+
+            return new JsonResult(kérdések);
+        }
+
+        [HttpGet]
+        [Route("questions/{sorszam}")]
+
+        public ActionResult M2(int sorszam)
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszam
+                          select x).FirstOrDefault();
+
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
+        }
+    }
+}
---
+++ HajosTeszt/HajosTeszt/wwwroot/minta.js
@@ -0,0 +1,36 @@
+﻿console.log("fut!")
+
+var faktorialis = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * i;
+    }
+    return er;
+}
+
+var faktorialisR = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    else {
+        return n * faktorialisR(n - 1)
+    }
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log(`${i} : ${faktorialisR(i)}`)
+}
+
+function szamitas() {
+    let n = document.getElementById("nTb").value
+  
+    let n2 = parseInt(n)
+    if (n2) {
+        let er = faktorialis(n)
+        document.getElementById("eredmenyDiv").innerText = er
+    }
+    else {
+        document.getElementById("eredmenyDiv").innerText = "?"
+    }
+
+}
